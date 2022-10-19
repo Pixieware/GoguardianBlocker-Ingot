975f282bfbc935e9b2c840b0fbf0a6363c4b99ce
frantene <112424233+frantene@users.noreply.github.com>
2022-10-19 08:34:31 -0500

    Bookmarklet.js
    Follow the steps to complete the Description

---
+++ Bookmarklet.js
@@ -0,0 +1,5 @@
+Go to link  https://fognetwork.github.io/Ingot
+then grab the "ingot" button and put in the bookmarks.
+then click "launch ingot"
+click redirect and it should take you to a page saying "Error 404"
+click the Ingot Bookmark and its done.
